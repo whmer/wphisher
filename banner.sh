a0b3c157506e3b5af852ba1b90d54d68a5f6899f
whmer <thekym50@gmail.com>
2024-08-11 21:45:22 -0300

    whmer

---
+++ banner.sh
@@ -0,0 +1,59 @@
+#                                                              
+#    .oooo.o  .oooo.    .oooo.o  .oooo.   ooo. .oo.  .oo.    .ooooo.  
+#   d88(  "8 `P  )88b  d88(  "8 `P  )88b  `888P"Y88bP"Y88b  d88' `88b 
+#   `"Y88b.   .oP"888  `"Y88b.   .oP"888   888   888   888  888ooo888 
+#   o.  )88b d8(  888  o.  )88b d8(  888   888   888   888  888    .o 
+#   8""888P' `Y888""8o 8""888P' `Y888""8o o888o o888o o888o `Y8bod8P' 
+#
+#     version        :  0.2.3
+#     author         :  sasame
+#     github         :  https://github.com/kym-sam 
+  
+#  Copyright (c) [2024] [whmerphisher] 
+
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# 3AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+#               ___________
+#              /            \
+#             |  Hey pretty /
+#        .--. /___________ _/
+#       |o_o | 
+#       |:_/ |
+#      //   \ \
+#     (|     | )
+#    /'\_   _/`\
+#    \___)=(___/ 
+
+#                    oooo                                             
+#                    `888                                             
+#   oooo oooo    ooo  888 .oo.   ooo. .oo.  .oo.    .ooooo.  oooo d8b 
+#    `88. `88.  .8'   888P"Y88b  `888P"Y88bP"Y88b  d88' `88b `888""8P 
+#     `88..]88..8'    888   888   888   888   888  888ooo888  888     
+#      `888'`888'     888   888   888   888   888  888    .o  888     
+#       `8'  `8'     o888o o888o o888o o888o o888o `Y8bod8P' d888b    
+# 
+#                                 o8o                .               
+#                                 `"'              .o8               
+#    .oooo.o  .ooooo.   .ooooo.  oooo   .ooooo.  .o888oo oooo    ooo 
+#   d88(  "8 d88' `88b d88' `"Y8 `888  d88' `88b   888    `88.  .8'  
+#   `"Y88b.  888   888 888        888  888ooo888   888     `88..8'   
+#   o.  )88b 888   888 888   .o8  888  888    .o   888 .    `888'    
+#   8""888P' `Y8bod8P' `Y8bod8P' o888o `Y8bod8P'   "888"     .8'     
+#                                                      .o..P'      
+#                                                      `Y8P'    
