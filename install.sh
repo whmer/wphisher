5c69bfa1ef84c9e85e49bfb67bddad7f71dab632
whmer <thekym50@gmail.com>
2024-08-25 21:47:01 -0300

    whmer

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Hello 👋
+
+pkg update && pkg upgrade
+pkg install which
+pkg install php
+pkg install nodejs -y
+pkg install nodejs-lts -y
+pkg install git -y
+pkg install ffmpeg -y
+npm install -g cfonts
+
+echo "[-] Concluding"
+
+chmod +x sam.sh
+bash sam.sh
